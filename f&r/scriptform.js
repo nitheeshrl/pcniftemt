dd523c4a46aef4049b68fd8f9b925e901340bb78
iacniftemt <rlnitheesh@gmail.com>
2024-12-15 00:09:26 +0530

    editable

--- f&r/scriptform.js
+++ f&r/scriptform.js
@@ -1,102 +1,23 @@
+function sidebari(){
+    var li = document.getElementById("sidebar").querySelectorAll("a");
 
-function getstatus1(){
-    getstatus("accept1","status1")
-}
-function clickto1(){
-    clickto("accept1","status1")
-}
+var nliclick =['userPopup()','','','','','','','','','','','passkey()','logoutPopup()'];
+var nlihref =['','dash.html','index.html','past.html','https://niftemt-placementcell-forms.pages.dev/B2K21%Brochure%Details%Update/user','new-placement-form.html','past-placement-forms.html','new-form.html','past-forms.html','all-users.html','change-pass-1.html','',''];
+var nlitext =['','Dashboards','Active Requests','Past Requests','Request Form','New Placement Form','Old Placement Forms','New Form','Old Forms','Students Details','Change Password','Create PassKey','Logout'];
 
-function getstatus2(){
-    getstatus("accept2","status2")
-}
-function clickto2(){
-    clickto("accept2","status2")
-}
-
-function getstatus3(){
-    getstatus("accept3","status3")
-}
-function clickto3(){
-    clickto("accept3","status3")
+for(var h=1;h<li.length;h++){
+    console.log(li[h].onclick)
+li[h].setAttribute("onclick",nliclick[h]);
+if(nlihref[h]!==''){
+li[h].href=nlihref[h];
 }
 
-function getstatus4(){
-    getstatus("accept4","status4")
+if (li[h].children.length==2){
+    li[h].children[1].textContent=nlitext[h]; 
 }
-function clickto4(){
-    clickto("accept4","status4")
+else{
+li[h].textContent=nlitext[h];
 }
-
-function getstatus5(){
-    getstatus("accept5","status5")
 }
-function clickto5(){
-    clickto("accept5","status5")
-}
-
-function getstatus6(){
-    getstatus("accept6","status6")
-}
-function clickto6(){
-    clickto("accept6","status6")
-}
-function getstatus7(){
-    getstatus("accept7","status7")
-}
-function clickto7(){
-    clickto("accept7","status7")
-}
-function getstatus8(){
-    getstatus("accept8","status8")
-}
-function clickto8(){
-    clickto("accept8","status8")
-}
-function getstatus9(){
-    getstatus("accept9","status9")
-}
-function clickto9(){
-    clickto("accept9","status9")
-}
-function getstatus10(){
-    getstatus("accept10","status10")
-}
-function clickto10(){
-    clickto("accept10","status10")
-}
-function getstatus11(){
-    getstatus("accept11","status11")
-}
-function clickto11(){
-    clickto("accept11","status11")
-}
-function getstatus12(){
-    getstatus("accept12","status12")
-}
-function clickto12(){
-    clickto("accept12","status12")
-}
-function getstatus13(){
-    getstatus("accept13","status13")
-}
-function clickto13(){
-    clickto("accept13","status13")
-}
-function getstatus14(){
-    getstatus("accept14","status14")
-}
-function clickto14(){
-    clickto("accept14","status14")
-}
-function getstatus15(){
-    getstatus("accept15","status15")
-}
-function clickto15(){
-    clickto("accept15","status15")
-}
-function getstatus16(){
-    getstatus("accept16","status16")
-}
-function clickto16(){
-    clickto("accept16","status16")
-}
+ }
+sidebari()
