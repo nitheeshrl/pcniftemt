d35e74239d23f55009b2d4a22862d4d94f6ddfc0
iacniftemt <rlnitheesh@gmail.com>
2024-12-15 00:10:57 +0530

    Update scriptform.js

--- f&r/scriptform.js
+++ f&r/scriptform.js
@@ -3,7 +3,7 @@ function sidebari(){
 
 var nliclick =['userPopup()','','','','','','','','','','','passkey()','logoutPopup()'];
 var nlihref =['','dash.html','index.html','past.html','https://niftemt-placementcell-forms.pages.dev/B2K21%Brochure%Details%Update/user','new-placement-form.html','past-placement-forms.html','new-form.html','past-forms.html','all-users.html','change-pass-1.html','',''];
-var nlitext =['','Dashboards','Active Requests','Past Requests','Request Form','New Placement Form','Old Placement Forms','New Form','Old Forms','Students Details','Change Password','Create PassKey','Logout'];
+var nlitext =['','Dashboard','Active Requests','Past Requests','Request Form','New Placement Form','Old Placement Forms','New Form','Old Forms','Students Details','Change Password','Create PassKey','Logout'];
 
 for(var h=1;h<li.length;h++){
     console.log(li[h].onclick)
