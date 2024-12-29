241d0706d03133d1eb981bab315c7558da76b14d
iacniftemt <rlnitheesh@gmail.com>
2024-12-29 13:49:46 +0530

    Update scriptform.js

--- f&r/scriptform.js
+++ f&r/scriptform.js
@@ -457,16 +457,6 @@ socket. emit( 'user_connected', newUserId);
 socket.on("disconnect", () => {
 console.log(socket.id); // undefined
 
-socket.on("connect_error", (err) => {
-// the reason of the error, for example "xhr poll error"
-console.log(err.message);
-
-// some additional description, for example the status code of the initial HTTP response
-console.log(err.description);
-
-// some additional context, for example the XMLHttpRequest object
-console.log(err.context);
-});
 });
 const handleFocus = async () => {
   socket. emit( 'user_connected', newUserId);
